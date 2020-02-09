c21a3975b1383b0b832196fcbf4fc1698ad4f8d2
Sohyun <56011398+sohyunbk@users.noreply.github.com>
2020-02-09 18:36:36 -0500

    Update 2.Change_toFastq.sh

--- 2.Change_toFastq.sh
+++ 2.Change_toFastq.sh
@@ -12,6 +12,6 @@ cd /scratch/sb14489/2.DAPseq/1.DownloadData
 List=(SRX3786361 SRX3786362 SRX3786363 SRX3786364 SRX3786365 SRX3786366 SRX3786367 SRX3786368 SRX3786369 SRX3786370 SRX3786371 SRX3786372 SRX3786373 SRX3786374)
 for item in ${List[@]};
 do
-echo fastq-dump "$item" 2> 2.ChangeName.log
-fastq-dump "$item" 2> 2.ChangeName.log
+echo fastq-dump "$item" #2> 2.ChangeName.log
+fastq-dump "$item" 2> #2.ChangeName.log
 done
